eeb42c58f9d6bff2ff944dd4ff4b4f797ed201ad
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2022-12-07 09:53:16 +0000

    initial commit 🌟

---
+++ app/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Notes App</title>
+        <meta name="description" content="Notes App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className='text-red-300 text-2xl'>Hii</h1>
+    </div>
+  )
+}
