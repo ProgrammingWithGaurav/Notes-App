0d5fb21d93ffb328f4801c6c477ae5400d25e5f8
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2022-12-13 10:32:51 +0000

    fixing errors

--- app/pages/profile.jsx
+++ app/pages/profile.jsx
@@ -4,7 +4,6 @@ import { NotesContext } from "../context/NotesContext";
 
 export default function Profile() {
   const {userLoggedInDetails} = useContext(NotesContext);
-  const {name, email, created_at, photoURL} = userLoggedInDetails;
   return (
     <>
       <Head>
@@ -15,7 +14,7 @@ export default function Profile() {
       <div className="overflow-hidden bg-white shadow sm:rounded-lg">
         <div className="px-4 py-5 sm:px-6">
           <h2 className="text-2xl font-medium leading-6 text-gray-900 flex flex-col items-center justify-center m-auto">
-            <img src={photoURL} className="ml-5 h-20 w-20 rounded-full" alt="profile Pic"/>
+            <img src={userLoggedInDetails?.photoURL} className="ml-5 h-20 w-20 rounded-full" alt="profile Pic"/>
             <p>Your Profile</p>
           </h2>
           <p className="mt-1 max-w-2xl text-sm text-gray-500">
@@ -27,7 +26,7 @@ export default function Profile() {
             <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt className="text-sm font-medium text-gray-500">Full name</dt>
               <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                    {name}
+                    {userLoggedInDetails?.name}
               </dd>
             </div>
             <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -35,13 +34,13 @@ export default function Profile() {
                 Email address
               </dt>
               <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                {email}
+                {userLoggedInDetails?.email}
               </dd>
             </div>
             <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt className="text-sm font-medium text-gray-500">About</dt>
               <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                 Account created on {created_at}
+                 Account created on {userLoggedInDetails?.created_at}
               </dd>
             </div>
           </dl>
